60721e8b25cc103561f043ff1acdef257b34a32d
Julia Johnson <julialj95@gmail.com>
2021-03-17 14:33:15 -0500

    purged building cache

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
-    process.env.DATABASE_URL || "postgresql://dunder_mifflin@localhost/noteful",
+    process.env.DATABASE_URL ||
+    "postgres://somestring:someuuid@ec2-23-23-184-76.compute-1.amazonaws.com:5432/d28qbhopgtqvcn",
 };
