7b61121cf2c67ebee73b50d055d863742386aee8
Julia Johnson <julialj95@gmail.com>
2021-03-17 19:02:58 -0500

    script edits

--- config.js
+++ config.js
@@ -2,6 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
-    process.env.DATABASE_URL ||
-    "postgres://somestring:someuuid@ec2-23-23-184-76.compute-1.amazonaws.com:5432/d28qbhopgtqvcn",
+    process.env.DATABASE_URL || "postgresql://dunder_mifflin@localhost/noteful",
 };
