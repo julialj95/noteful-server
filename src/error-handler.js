7df21136787d7d0174cd0b283a3d36b5242b7b6c
Julia Johnson <julialj95@gmail.com>
2021-03-19 18:06:05 -0500

    changed console error

--- src/error-handler.js
+++ src/error-handler.js
@@ -2,10 +2,10 @@ const { NODE_ENV } = require("./config.js");
 
 function errorHandler(error, req, res, next) {
   let response;
+  console.error(error);
   if (NODE_ENV === "production") {
     response = { error: { message: "server error" } };
   } else {
-    console.error(error);
     response = { message: error.message, error };
   }
   res.status(500).json(response);
